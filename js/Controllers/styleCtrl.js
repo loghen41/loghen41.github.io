464c01c7183a8e88086923024d6c80d05bd67851
loganhendricks <lhendricks88@gmail.com>
2016-08-03 10:14:29 -0600

    re-inserted the delete property

--- js/Controllers/styleCtrl.js
+++ js/Controllers/styleCtrl.js
@@ -3,7 +3,6 @@ angular.module('App')
     .controller('styleCtrl', function(mainService, $translate) {
         this.$onInit = function() {
             this.style = mainService.getSelectedStyle();
-            console.log(this.style);
         };
         
     });
